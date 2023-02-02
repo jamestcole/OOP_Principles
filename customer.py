e7784475a1ce6f639e953db80cdf3efc758e57f1
James Cole <jamestcole@yahoo.co.uk>
2023-02-02 10:29:06 +0000

    main

---
+++ customer.py
@@ -0,0 +1,6 @@
+class Customer:
+    def __init__(self, fname, lname):
+        self.firstName = fname
+        self.lastName - lname
+    def print(self):
+        print(f"Full name: {self.firstName} {self.lastName}")
